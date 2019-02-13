4157ca59b8061b7451da353e8aee7d2beae3fb90
Nessa <vanessaLie98@gmail.com>
2019-02-13 10:55:32 +0700

    add hellow word text

--- index.php
+++ index.php
@@ -8,9 +8,11 @@
 <body>
     <H1>INI pake git</H1>
     <? php
-        for($i=0; $ < 5; i++){
-            echo("for $i");
-        }
+      for($i=0 ; $i < 5 ; $i++){
+          echo("i for $i");
+          echo("hellow world");
+      }
+     
     ?>
 </body>
 </html>
